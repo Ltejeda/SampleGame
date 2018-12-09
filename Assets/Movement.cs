0820a24164b54a46a20c128c48c6a01b13f9a35b
Ltejeda <the01game@yahoo.com>
2018-12-08 22:51:24 -0800

    first commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Animator _animator;
+    private float _speed = 0.01f;
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.SetFloat("Speed", _speed);
+        _animator.SetBool("Jumping", false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("1"))
+        {
+            _animator.SetBool("Jumping", true);
+            _speed = 0.01f;
+            _animator.SetFloat("Speed", _speed);
+        }
+        else if (Input.GetKey("2"))
+        {
+            _animator.SetBool("Jumping", false);
+            _speed = 0.5f;
+            _animator.SetFloat("Speed", _speed);
+        }
+        else if (Input.GetKey("3"))
+        {
+            _animator.SetBool("Jumping", false);
+            _speed = 0.01f;
+            _animator.SetFloat("Speed", _speed);
+        }
+    }
+}
---
+++ Assets/WanderingAI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 3.0f; public float obstacleRange = 5.0f;
+    private bool _alive;
+    private Animator _animator;
+    private float _speed = 0.01f;
+    [SerializeField] private GameObject fireballPrefab;
+    private GameObject _fireball;
+    void Start() { _alive = true;
+        _animator = GetComponent<Animator>();
+        _animator.SetFloat("Speed", _speed);
+        _animator.SetBool("Jumping", false);
+    }
+
+    void Update()
+    {
+        if (_alive) {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            _animator.SetBool("Jumping", false);
+            _speed = 0.5f;
+            _animator.SetFloat("Speed", _speed);
+        }
+     
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.SphereCast(ray, 0.75f, out hit))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.GetComponent<PlayerCharacter>()) {
+                if (_fireball == null) {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                }
+            }
+            else if (hit.distance < obstacleRange) {
+                float angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }
+    }
+    public void SetAlive(bool alive) { _alive = alive; }
+}
